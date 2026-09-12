50d994667d38fd05f0b7aa1d30df5f21e70e547f
HatsuHodowa <knineteen19@pm.me>
2026-09-12 16:07:27 -0400

    test dialog system

---
+++ language-game/scripts/dialog/dialog_player.gd
@@ -0,0 +1,53 @@
+extends CanvasLayer
+
+@export_file var scene_text_file
+
+var scene_text = {}
+var selected_text = []
+var in_progress = false
+
+@onready var background: TextureRect = $Background
+@onready var text_label: Label = $TextLabel
+
+func _ready():
+	background.visible = false
+	scene_text = load_scene_text()
+	SignalBus.connect("display_dialog", on_display_dialog)
+
+func load_scene_text() -> Dictionary:
+	var file = FileAccess.open(scene_text_file, FileAccess.READ)
+	var content = file.get_as_text()
+	
+	var json = JSON.new()
+	var error = json.parse(content)
+	if error == OK:
+		return json.data
+	else:
+		print("JSON Parse Error: ", json.get_error_message())
+		return {}
+	
+func show_text():
+	text_label.text = selected_text.pop_front()
+	
+func next_line():
+	if selected_text.size() > 0:
+		show_text()
+	else:
+		finish()
+		
+func finish():
+	text_label.text = ""
+	background.visible = false
+	in_progress = false
+	get_tree().paused = false
+	
+func on_display_dialog(text_key):
+	if in_progress:
+		next_line()
+	else:
+		get_tree().paused = true
+		background.visible = true
+		in_progress = true
+		selected_text = scene_text[text_key].duplicate()
+		show_text()
+		
